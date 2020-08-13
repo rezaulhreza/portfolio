b511373cea7502ccfbc6ea25fa2a6efd77cf6c99
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2020-08-14 00:58:15 +0100

    import all links

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import{Layout,Header,Navigation,Drawer, Content} from 'react-mdl';
 import Main from './components/main';
+import {Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
