9f10193c579ece3912a937bece68d01021f280cb
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2020-08-14 06:45:06 +0100

    landing page created

--- portfolio/src/components/landingpage.js
+++ portfolio/src/components/landingpage.js
@@ -13,38 +13,44 @@ import {Grid, Cell} from 'react-mdl';
                 <h1>Full Stack Web Developer</h1>
                 <div className="landing-skills">
                 <div className="FrontEnd">
-                <p><i class="fas fa-code"></i> HTML</p>
-                <p><i class="fab fa-css3-alt"></i> CSS</p>
-                <p><i class="fab fa-js-square"></i> JavaScript</p>
-                <p><i class="fab fa-bootstrap"></i> Bootstrap</p>
+                <p><i className="fas fa-code"></i> HTML</p>
+                <p><i className="fab fa-css3-alt"></i> CSS</p>
+                <p><i className="fab fa-js-square"></i> JavaScript</p>
+                <p><i className="fab fa-bootstrap"></i> Bootstrap</p>
                 </div>
                 <div className="FrontEnd">
-                <p><i class="fab fa-react"></i> React</p>
-                <p><i class="fab fa-react"></i> React Native</p>
-                <p><i class="fab fa-node-js"></i> NodeJS</p>
+                <p><i className="fab fa-react"></i> React</p>
+                <p><i className="fab fa-react"></i> React Native</p>
+                <p><i className="fab fa-node-js"></i> NodeJS</p>
                 <p>React Redux</p>
                 </div>
                 <div className="FrontEnd">
-                <p><i class="fas fa-code-branch"></i> Firebase</p>
-                <p><i class="fas fa-code-branch"></i> Heroku</p>
-                <p><i class="fab fa-cpanel"></i> cPanel</p>
+                <p><i classNamae="fas fa-code-branch"></i> Firebase</p>
+                <p><i className="fas fa-code-branch"></i> Heroku</p>
+                <p><i className="fab fa-cpanel"></i> cPanel</p>
                 <p>MongoDB</p>
                 </div>
                 <div className="FrontEnd">
                 <p><i class="fas fa-database"></i> Oracle SQL</p>
-                <p><i class="fab fa-java"></i> Java</p>
-                <p><i class="fab fa-python"></i> Python- Flask</p>
+                <p><i className="fab fa-java"></i> Java</p>
+                <p><i className="fab fa-python"></i> Python- Flask</p>
                 <p>Python- Django</p>
                 </div>
                 </div>
-                <div className="Social-links">
+                {/*linlkedin */}
+                <div className="social-links">
                 <a href="https://www.rezaulhreza.co.uk" rel="noopener noreferrer" target="_blank">
-                <i class="fab fa-linkedin-in" aria-hidden="true"></i>
+                <i className="fab fa-linkedin" aria-hidden="true"></i></a>
+                {/* Github*/}
+                <a href="https://www.rezaulhreza.co.uk" rel="noopener noreferrer" target="_blank">
+                <i className="fab fa-github-square" aria-hidden="true"></i>
+                </a>
+                {/*Freecodecamp */}
+                <a href="https://www.rezaulhreza.co.uk" rel="noopener noreferrer" target="_blank">
+                <i className="fab fa-free-code-camp" aria-hidden="true"></i>
                 </a>
                 </div>
                 </div>
-
-
               </Grid>
             </div>
         )
