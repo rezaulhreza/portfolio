444ac2b60b19fd3dbc3c9b7ec62bf4b991bd5d37
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2020-08-14 03:01:36 +0100

    icons on landing page

--- portfolio/src/components/landingpage.js
+++ portfolio/src/components/landingpage.js
@@ -13,25 +13,31 @@ import {Grid, Cell} from 'react-mdl';
                 <h1>Full Stack Web Developer</h1>
                 <div className="landing-skills">
                 <div className="FrontEnd">
-                <p>HTML</p>
-                <p>CSS</p>
-                <p>JavaScript</p>
+                <p><i class="fas fa-code"></i> HTML</p>
+                <p><i class="fab fa-css3-alt"></i> CSS</p>
+                <p><i class="fab fa-js-square"></i> JavaScript</p>
+                <p><i class="fab fa-bootstrap"></i> Bootstrap</p>
                 </div>
                 <div className="FrontEnd">
-                <p>React</p>
-                <p>React Native</p>
-                <p>NodeJS</p>
+                <p><i class="fab fa-react"></i> React</p>
+                <p><i class="fab fa-react"></i> React Native</p>
+                <p><i class="fab fa-node-js"></i> NodeJS</p>
                 </div>
                 <div className="FrontEnd">
-                <p>Firebase</p>
-                <p>Heroku</p>
-                <p>cPanel</p>
+                <p><i class="fas fa-code-branch"></i> Firebase</p>
+                <p><i class="fas fa-code-branch"></i> Heroku</p>
+                <p><i class="fab fa-cpanel"></i> cPanel</p>
                 </div>
                 <div className="FrontEnd">
-                <p>Oracle SQL</p>
-                <p>Java</p>
-                <p>Flask</p>
+                <p><i class="fas fa-database"></i> Oracle SQL</p>
+                <p><i class="fab fa-java"></i> Java</p>
+                <p><i class="fab fa-python"></i> Python- Flask</p>
                 </div>
+                </div>
+                <div className="Social-links">
+                <a href="https://www.rezaulhreza.co.uk" rel="noopener noreferrer" target="_blank">
+                <i class="fab fa-linkedin-in" aria-hidden="true"></i>
+                </a>
                 </div>
                 </div>
 
